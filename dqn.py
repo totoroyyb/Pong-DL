b85e7c13506a09b42705a62a324614dd42d19fb2
TotoroQ <totoroq@outlook.com>
2020-03-11 20:47:06 -0700

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -93,7 +93,7 @@ def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
     if torch.cuda.is_available():
         loss = loss.cuda()
     end_time = time.perf_counter()
-    print(f"Finish to computer loss, takes " + str(end_time - start_time) + " seconds")
+    print("Finish to computer loss, takes " + str(end_time - start_time) + " seconds")
     
     return loss
 
@@ -119,7 +119,7 @@ class ReplayBuffer(object):
         next_state = list(map(lambda x: x[3], samples))
         done = list(map(lambda x: x[4], samples))
         end_time = time.perf_counter()
-        print(f"Sample End, takes " + str(end_time - start_time) + "seconds\n")
+        print("Sample End, takes " + str(end_time - start_time) + "seconds\n")
 
         return state, action, reward, next_state, done
 
